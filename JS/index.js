80702c8ed5b383a69c62152e136cf22031d0627a
PC <kim000183@naver.com>
2022-05-10 23:04:59 +0900

    계산기 수정중

--- JS/index.js
+++ JS/index.js
@@ -1,28 +1,72 @@
+// let oper ;
+// let num1 ;
+
+// const refucn = (ts) => {
+//     result.value += ts;
+// }
+
+// const cal = (symbol) => {
+//     num1 = document.getElementById('result').value;
+//     oper = symbol;
+//     return document.getElementById('result').value = "";
+// }
+
+// const opeven = () => {
+//     let num2 = document.getElementById('result').value;
+//     let result ;
+    
+//     switch(oper){
+//         case '+' : result = Number(num1) + Number(num2)
+//         document.getElementById('result').value = result;
+//         break;
+//         case '-' : result = Number(num1) - Number(num2)
+//         document.getElementById('result').value = result;
+//         break;
+//         case '*' : result = Number(num1) * Number(num2)
+//         document.getElementById('result').value = result;
+//         break;
+//         case '/' : result = Number(num1) / Number(num2)
+//         document.getElementById('result').value = result;
+//         break;
+//     }
+//     num1 = result
+// }
+// const clear02 = () => {
+//     num1 = 0;
+//     return document.getElementById('result').value = "";
+// }
+
 let oper ;
 let num1 ;
+let num2 ;
 
 const refucn = (ts) => {
     result.value += ts;
+    if(num1 >= 0 || num1 <= 0){
+        num2 = ts;
+        console.log(num2);
+        return false
+    }
+    num1 = ts;
 }
 
 const cal = (symbol) => {
-    num1 = document.getElementById('result').value;
     oper = symbol;
-    return document.getElementById('result').value = "";
+    return result.value += symbol;
 }
 
 const opeven = () => {
-    let num2 = document.getElementById('result').value;
     let result ;
     
     switch(oper){
-        case '+' : result = Number(num1) + Number(num2)
+        case '+' : result = num1 + num2
+        console.log(num1, num2);
         document.getElementById('result').value = result;
         break;
         case '-' : result = Number(num1) - Number(num2)
         document.getElementById('result').value = result;
         break;
-        case '*' : result = Number(num1) * Number(num2)
+        case 'X' : result = Number(num1) * Number(num2)
         document.getElementById('result').value = result;
         break;
         case '/' : result = Number(num1) / Number(num2)
@@ -35,4 +79,5 @@ const clear02 = () => {
     num1 = 0;
     return document.getElementById('result').value = "";
 }
+
 
