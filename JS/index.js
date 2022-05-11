be5460658cf97880c67e49abf6f514ad6520a944
PC <kim000183@naver.com>
2022-05-11 22:33:58 +0900

    두자릿수 계산 진행중

--- JS/index.js
+++ JS/index.js
@@ -1,41 +1,3 @@
-// let oper ;
-// let num1 ;
-
-// const refucn = (ts) => {
-//     result.value += ts;
-// }
-
-// const cal = (symbol) => {
-//     num1 = document.getElementById('result').value;
-//     oper = symbol;
-//     return document.getElementById('result').value = "";
-// }
-
-// const opeven = () => {
-//     let num2 = document.getElementById('result').value;
-//     let result ;
-    
-//     switch(oper){
-//         case '+' : result = Number(num1) + Number(num2)
-//         document.getElementById('result').value = result;
-//         break;
-//         case '-' : result = Number(num1) - Number(num2)
-//         document.getElementById('result').value = result;
-//         break;
-//         case '*' : result = Number(num1) * Number(num2)
-//         document.getElementById('result').value = result;
-//         break;
-//         case '/' : result = Number(num1) / Number(num2)
-//         document.getElementById('result').value = result;
-//         break;
-//     }
-//     num1 = result
-// }
-// const clear02 = () => {
-//     num1 = 0;
-//     return document.getElementById('result').value = "";
-// }
-
 let oper ;
 let num1 ;
 let num2 ;
@@ -47,17 +9,17 @@ const refucn = (ts) => {
         console.log(num2);
         return false
     }
-    num1 = ts;
 }
 
 const cal = (symbol) => {
+    num1 = Number(document.getElementById("result").value);
+    console.log(num1)
     oper = symbol;
     return result.value += symbol;
 }
 
 const opeven = () => {
     let result ;
-    
     switch(oper){
         case '+' : result = num1 + num2
         console.log(num1, num2);
@@ -75,6 +37,7 @@ const opeven = () => {
     }
     num1 = result
 }
+
 const clear02 = () => {
     num1 = undefined;
     console.log(num1);
