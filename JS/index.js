1170918dc0fa0a920256722a26ae88fc6c579c8c
PC <kim000183@naver.com>
2022-05-13 22:43:51 +0900

    계산기 수정

--- JS/index.js
+++ JS/index.js
@@ -1,27 +1,29 @@
 let oper ;
 let num1 ;
-let num2 ;
+// let num2 ;
 
 const refucn = (ts) => {
     result.value += ts;
-    if(num1 >= 0 || num1 <= 0){
-        num2 = ts;
-        console.log(num2);
-        return false
-    }
+    // if(num1 >= 0 || num1 <= 0){
+    //     num2 = ts;
+    //     console.log(num2);
+    //     return false
+    // }
 }
 
 const cal = (symbol) => {
     num1 = Number(document.getElementById("result").value);
     console.log(num1)
     oper = symbol;
-    return result.value += symbol;
+    // return result.value += symbol;
+    return document.getElementById("result").value = "";
 }
 
 const opeven = () => {
+    let num2 = parseFloat(document.getElementById("result").value);
     let result ;
     switch(oper){
-        case '+' : result = num1 + num2
+        case '+' : result = Number(num1) + Number(num2)
         console.log(num1, num2);
         document.getElementById('result').value = result;
         break;
