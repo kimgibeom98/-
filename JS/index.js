9cbc88dd422bb7025b33eeed770fc2af8d295b78
PC <kim000183@naver.com>
2022-06-14 23:25:04 +0900

    전체 수정

--- JS/index.js
+++ JS/index.js
@@ -1,101 +1,114 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    // document.getElementById('view').innerHTML = "<input type='text' id='result02'> \n  <input type='text' id='result'> \n   <div class='num1-box'>\n  <input type='button' value='AC' onclick='clear02()'> \n  <input class='op-cr' type='button' value='%' onclick='cal('/')''>\n</div>"
-   
-    //     const a = new Array(10).fill(false).map((_, index) => `<input type="text" value=${index} onclick="refucn(${index})"`)       
-    // document.getElementById('view').innerHTML ="<div class='num2-box'> \n <input type='button' value='0' onclick='refucn(0)'> \n  <input type='button' value='.' onclick='refucn('.')''> \n  <input class='op-cr' type='button' value='='' onclick='opeven()'> \n </div>"
+  // document.getElementById('view').innerHTML = "<input type='text' id='result02'> \n  <input type='text' id='result'> \n   <div class='num1-box'>\n  <input type='button' value='AC' onclick='clear02()'> \n  <input class='op-cr' type='button' value='%' onclick='cal('/')''>\n</div>"
+
+  //     const a = new Array(10).fill(false).map((_, index) => `<input type="text" value=${index} onclick="refucn(${index})"`)       
+  // document.getElementById('view').innerHTML ="<div class='num2-box'> \n <input type='button' value='0' onclick='refucn(0)'> \n  <input type='button' value='.' onclick='refucn('.')''> \n  <input class='op-cr' type='button' value='='' onclick='opeven()'> \n </div>"
 
 });
-    const arr = []
-    document.addEventListener("keydown", (e) => {
-        const valuemap  = ["0","1","2","3","4","5","6","7","8","9",'-','*','/','+','Enter'];
-        const keye = valuemap.find(function(i){
-            return i === e.key
-        });
-        if(keye){
-            result.value += keye;
-            result02.value += keye;
-            arr.push(['-','*','/','+','Enter'].includes(keye) ? keye : Number(keye))
-            if(keye === "Enter"){
-                for(let j = 0; j < arr.length; j++){
-                    if(arr[j] === '*' || arr[j] === '/' ){
-                        const firarr =  arr.splice(j - 1, j + 2); 
-                        firarr.splice(-1,1);
-                        const fiindex = firarr.findIndex((i) => ['*','/'].includes(i))
-                        const [fir, op, la] = firarr.splice(fiindex - 1, fiindex + 2)
-                        switch(op){
-                            case '*' :
-                                arr.push(fir * la);
-                                break;
-                            case '/' :
-                                arr.push(fir / la);
-                                break;
-                
-                        }
-                    }
-                }
-                const fiindex = arr.findIndex((i) => ['-','+'].includes(i))
-                const [fir, op, la] = arr.splice(fiindex - 1, fiindex + 2)
-                switch(op){
-                    case '+' :
-                        result = fir + la;
-                        break;
-                    case '-' :
-                        result = fir - la;
-                        break;
-        
-                }
-                document.getElementById('result').value = result;
-                document.getElementById('result02').value = result;
-            }
-           
-        } 
-    })
+const arr = []
+const temp = [];
+document.addEventListener("keydown", (e) => {
+  const valuemap = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", '-', '*', '/', '+', 'Enter'];
+  const keye = valuemap.find(function (i) {
+    return i === e.key
+  });
 
-let oper ;
-let num1 ;
+  if (keye) {
+    result.value += keye;
+    result02.value += keye;
+
+    if (['-', '*', '/', '+', 'Enter'].includes(keye)) {
+      arr.push(Number(temp.join("")), keye);
+    } else {
+      temp.push(keye);
+    }
+
+
+    if (keye === "Enter") {
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j] === '*' || arr[j] === '/') {
+          const firarr = arr.splice(j - 1, j + 2);
+          firarr.splice(-1, 1);
+          const firstCaseIndex = firarr.findIndex((i) => ['*', '/'].includes(i))
+          const [fir, op, la] = firarr.splice(firstCaseIndex - 1, firstCaseIndex + 2)
+        //   switch (op) {
+        //     case '*': b
+        //       arr.push(fir * la);
+        //       break;
+        //     case '/':
+        //       arr.push(fir / la);
+        //       break;
+        //   }
+          arr.push(op === "*" ? fir * la : fir / la);
+        }
+      }
+      const secondCaseIndex = arr.findIndex((i) => ['-', '+'].includes(i));
+      const [fir, op, la] = arr.splice(secondCaseIndex - 1, secondCaseIndex + 2);
+    //   switch (op) {
+    //     case '+':
+    //       result = fir + la;
+    //       break;
+    //     case '-':
+    //       result = fir - la;
+    //       break;
+
+    //   }
+    arr.push(op === "+" ? fir + la : fir - la);
+      render()
+    }
+
+  }
+})
+function render() {
+  document.getElementById('result').value = result;
+  document.getElementById('result02').value = result;
+}
+let oper;
+let num1;
 const show = (tx) => {
-    result.value += tx;
-    result02.value += tx;
+  result.value += tx;
+  result02.value += tx;
 
 }
 
 const operator = (op) => {
-    num1 = Number(document.getElementById("result").value);
-    console.log(num1)
-    oper = op;
-    result02.value += op;
-    return document.getElementById("result").value = "";
+  num1 = Number(document.getElementById("result").value);
+  console.log(num1)
+  oper = op;
+  result02.value += op;
+  return document.getElementById("result").value = "";
 }
 
 const calculate = () => {
-    let num2 = parseFloat(document.getElementById("result").value);
-    let result ;
-    switch(oper){
-        case '+' :
-            result = Number(num1) + Number(num2)
-            break;
-        case '-' :
-            result = Number(num1) - Number(num2)
-            break;
-        case 'X' :
-            result = Number(num1) * Number(num2)
-            break;
-        case '/' :
-            result = Number(num1) / Number(num2)
-            break;
-    }
-    document.getElementById('result').value = result;
-    document.getElementById('result02').value = result;
-    num1 = result
+  const num2 = parseFloat(document.getElementById("result").value);
+  let result;
+  switch (oper) {
+    case '+':
+      result = Number(num1) + Number(num2)
+      break;
+    case '-':
+      result = Number(num1) - Number(num2)
+      break;
+    case 'X':
+      result = Number(num1) * Number(num2)
+      break;
+    case '/':
+      result = Number(num1) / Number(num2)
+      break;
+  }
+  render();
+  num1 = result
 
 }
 
-const clear = () => {
-    num1 = undefined;
-    document.getElementById('result').value = "";
-    return document.getElementById('result02').value = "";
+function clearInputField() {
+//   console.log(document.getElementById('result').value);
+  num1 = undefined;
+  console.log(document.getElementById('result').value);
+  document.getElementById('result').value = "22";
+  console.log(document.getElementById('result').value);
+  document.getElementById('result02').value = "";
+
 }
-
-
