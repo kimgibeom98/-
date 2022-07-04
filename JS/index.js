212c7088e4b3c564632d7f6d0a185458642975e9
PC <kim000183@naver.com>
2022-07-04 22:43:40 +0900

    6*6Enter후 / 해결

--- JS/index.js
+++ JS/index.js
@@ -15,6 +15,7 @@ document.addEventListener("keydown", (e) => {
     });
 
     if(keye){
+      
         result.value += keye;
         result02.value += keye;
     }
@@ -22,6 +23,7 @@ document.addEventListener("keydown", (e) => {
       calculaterResult(keye || entkey)
 
 })
+
 
 // 마우스 클릭 event
 function cognizeClick(c){
@@ -48,12 +50,12 @@ function viewResult(to) {
 // 연속된 숫자 및 연산기호 받아서 arr변수에 담기
 function insertKey(keye){
     if (['-', '*', '/', '+', 'Enter','='].includes(keye)) {
+      // console.log(cnsctNmbrs)
         arr.push(Number(cnsctNmbrs.join("")), keye);
+        // console.log(arr);
         cnsctNmbrs = [];
       }else {
         cnsctNmbrs.push(keye);
-
-        
       }
 }
 
@@ -71,9 +73,10 @@ function hasPlusminers(){
 function hasMultiplydivision(){
     for(let i = 0; i < arr.length; i++){
         if(arr[i] === '*' || arr[i] === '/' ){
+          return true
         }
       }
-      return true;
+      return false;
 }
 
 // 곱하기 우선 연산 및 곱하기연산
@@ -85,16 +88,20 @@ function repeatMultiplydivision(){
             }
         let firarr = arr.splice(firstCaseIndex -1 ,3);
         const [fir, op, la] = firarr
+        console.log([fir, op, la]);
         if(hasPlusminers()){
-          showresult = (op === '*' ? fir * la : fir / la);
+            showresult = (op === '*' ? fir * la : fir / la);
             arr.splice(firstCaseIndex -1, 0, showresult)
-        }else if(hasMultiplydivision()){
+        }else{
           showresult = (op === '*' ? fir * la : fir / la);
+          // console.log(showresult)
           if(hasMultiplydivision()){
+            // console.log(showresult)
             multiplydivision.push(showresult)
             calculateMultiplydivision();
             return false
           }else{
+          
             viewResult(showresult)
           }
         }
@@ -124,6 +131,7 @@ function calculatePlusminers(){
 
 //곱하기 연산
 function calculateMultiplydivision(){
+  console.log(arr)
   for(let i = 0; i< arr.length; i++){
     if(arr[i] === '*' || arr[i] === '/'){
       
@@ -142,9 +150,11 @@ function calculateMultiplydivision(){
 function calculaterResult(keye){
     if(keye === "Enter" || keye === "=" ){
         arr.splice(-1,1)
+        // console.log(arr);
         repeatMultiplydivision();
         arr = [];
         cnsctNmbrs.push(showresult);
+    
        
     }
 }
