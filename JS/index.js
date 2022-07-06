82e48e1438e24721d6ee20400eca026ac30b6220
PC <kim000183@naver.com>
2022-07-06 22:05:51 +0900

    tets

--- JS/index.js
+++ JS/index.js
@@ -4,7 +4,7 @@ const multiplydivision = []
 let showresult;
 let keye;
 let val;
-
+s
 document.addEventListener("keydown", (e) => {
 
     const valuemap = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", '-', '*', '/', '+','.'];
