33a0c9cc3c4a667e6861706c06d2ab47265fdeb5
PC <kim000183@naver.com>
2022-07-08 22:26:02 +0900

    연속 연산 수정완료 코드정리전

--- JS/index.js
+++ JS/index.js
@@ -6,9 +6,9 @@ let resultnumber;
 let val;
 let reset = '';
 
-const valuemap = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", '-', '*', '/', '+','.'];
+const valuemap = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", '-', '*', '/', '+','.','='];
 const entval = ['Enter','Backspace'];
-const clickresult = '=';
+// const clickresult = '=';
 
 // 키보드 event
 document.addEventListener("keydown", (e) => {
@@ -31,13 +31,15 @@ function cognizeClick(c){
     val = valuemap.find(function (i) {
       return i === c
     });
-    insertKey(val || clickresult);
-    calculaterResult(val || clickresult)
+
+    insertKey(val);
+    calculaterResult(val)
     render()
 }
 
 // UI로 계산식 보여주는 함수
 function render(){
+  // console.log(history)
   document.getElementById('result02').value = history.join('');
 }
 
@@ -45,26 +47,39 @@ function render(){
 function viewResult(to) {
   document.getElementById('result').value = to;
   history.push(resultnumber)
-  console.log(history)
 }
 
 
-// 연속된 숫자 및 연산기호 받아서 arr변수에 담기
+
+// 배열 계산식 보여주는 함수
 function insertKey(keye){
-      if(valuemap.includes(keye)){
-        history.push(keye);
-        console.log(history)
+    const oparry = ['-', '*', '/', '+','Enter','=']
+      if(valuemap.includes(keye) || entval.includes(keye)){
+        if(oparry.includes(history[history.length -1])){
+          if(oparry.includes(keye)){
+            
+          }else{
+            combineArray(keye);
+          }
+        }else{
+         
+          combineArray(keye);
+        }
       }
-     
-     if(['-', '*', '/', '+','Enter','='].includes(keye)) {
-        arr.push(Number(cnsctNmbrs.join("")), keye);
-        cnsctNmbrs.splice(0, cnsctNmbrs.length);
-      }else {
-        cnsctNmbrs.push(keye);
-      }
-      if(keye === 'Backspace'){
-        arr.push(Number(cnsctNmbrs.join("")), keye);
-      }
+}
+
+// 연속된 숫자 및 연산기호 받아서 arr변수에 담는 함수
+function combineArray(keye){
+    history.push(keye);
+    if(['-', '*', '/', '+','Enter','='].includes(keye)) {
+      arr.push(Number(cnsctNmbrs.join("")), keye);
+      cnsctNmbrs.splice(0, cnsctNmbrs.length);
+    }else {
+      cnsctNmbrs.push(keye);
+    }
+    if(keye === 'Backspace'){
+      arr.push(Number(cnsctNmbrs.join("")), keye);
+    }
 }
 
 // backspace 지우기 Even
@@ -73,11 +88,11 @@ function eraseBackkey(entkey){
 
     if(entkey === 'Backspace'){
       result02.value = txtvalue02.substr(0, txtvalue02.length - 1);
-      history.pop(); 
-      cnsctNmbrs.pop();
-      arr.pop();
-      cnsctNmbrs.pop()
-      arr.pop()
+      for(let i = 0; i < 2; i++){
+        history.pop();  
+        cnsctNmbrs.pop()
+        arr.pop() 
+      }
     }
    
 }
@@ -121,7 +136,6 @@ function repeatMultiplydivision(){
             calculateMultiplydivision();
             return false
           }else{
-          
             viewResult(resultnumber)
           }
         }
@@ -174,7 +188,6 @@ function calculateMultiplydivision(){
 function calculaterResult(keye){
     if(keye === "Enter" || keye === "=" ){
         history.splice(0, history.length);
-        console.log(history)
         arr.splice(-1,1)
         repeatMultiplydivision();
         
