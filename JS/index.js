02b6a9a33b02f9fb542895c65b06a53f37b1b2e2
PC <kim000183@naver.com>
2022-07-15 19:02:12 +0900

    마우스 입력호 키보드 엔터 오류해결

--- JS/index.js
+++ JS/index.js
@@ -3,64 +3,50 @@ const cnsctNmbrs = [];
 const history = [];
 
 let resultnumber;
-let val;
-let reset = '';
 
-const valuemap = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", '-', '*', '/', '+','.','='];
-const entval = ['Enter','Backspace'];
+const valuemap = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", '-', '*', '/', '+','.'];
+const entval = ['Enter','Backspace','='];
 // const clickresult = '=';
 
 // 키보드 event
 document.addEventListener("keydown", (e) => {
-
-  if(valuemap.includes(e.key)){
-
-    insertKey(e.key);
-    calculaterResult(e.key);
-    render();
-
-  }else if(entval.includes(e.key)){
-    console.log(e.key)
-    insertKey(e.key);
-    eraseBackkey(e.key);
-    calculaterResult(e.key);
-    render();
-    console.log(e.key)
-  }
-
+  executefun(e.key);
 })
 
 // 마우스 클릭 event
 function cognizeClick(c){
-  console.log(c)
-    val = valuemap.find(function (i) {
-      return i === c
-    });
-    
-    insertKey(val);
-    calculaterResult(val)
-    render()
-  
+    executefun(c);
+}
+
+// 함수 실행
+function executefun(factor){
+    if(valuemap.includes(factor)){
+      insertKey(factor);
+      calculaterResult(factor);
+      render();
+
+    }else if(entval.includes(factor)){
+      insertKey(factor);
+      eraseBackkey(factor);
+      calculaterResult(factor);
+      render();
+    }
 }
 
 // UI로 계산식 보여주는 함수
 function render(){
-
   document.getElementById('result02').value = history.join('');
-  
 }
 
 // UI로 결과값 보여주는 함수
 function viewResult(to) {
+  history.push(resultnumber) 
   document.getElementById('result').value = to;
-  history.push(resultnumber)
- 
 }
 
 // 배열 계산식 보여주는 함수
 function insertKey(keye){
-    const oparry = ['-', '*', '/', '+','Enter','=']
-    console.log(keye)
+    const oparry = ['-', '*', '/', '+','Enter','=','.']
       if(valuemap.includes(keye) || entval.includes(keye)){
         if(oparry.includes(history[history.length -1])){
           if(oparry.includes(keye)){
@@ -72,7 +58,6 @@ function insertKey(keye){
           combineArray(keye);
         }
       }
-      console.log(e.key)
 }
 
 // 연속된 숫자 및 연산기호 받아서 arr변수에 담는 함수
@@ -88,7 +73,6 @@ function combineArray(keye){
       arr.push(Number(cnsctNmbrs.join("")), keye);
     }
 }
-
 
 
 // 입력값에 플러스 or 마이너스가 있는지 확인
@@ -193,14 +177,12 @@ function eraseBackkey(entkey){
 function calculaterResult(keye){
     if(keye === "Enter" || keye === "=" ){
         history.splice(0, history.length);
-        console.log(history)
         arr.splice(-1,1)
         repeatMultiplydivision();
-        
         arr.splice(0, arr.length);
         cnsctNmbrs.push(resultnumber);
-      
-    }
+        event.preventDefault()
+      }
 }
 
 // 화면 초기화
