e4c10de49fc7de1a6130c18d1416c6db8b5c81ed
PC <kim000183@naver.com>
2022-11-05 21:18:04 +0900

    정렬

--- index.js
+++ index.js
@@ -20,52 +20,52 @@ function cognizeClick(clvalue) {
 
 // 함수 실행
 function executefun(value) {
-    if (valuemap.includes(value) || entval.includes(value)) {
-      insertKey(value);
-      render();
-    }
+  if (valuemap.includes(value) || entval.includes(value)) {
+    insertKey(value);
+    render();
+  }
 }
 
 // UI로 계산식 보여주는 함수
 function render() {
-    document.getElementById('result02').value = history.join('');
+  document.getElementById('result02').value = history.join('');
 }
 
 // UI로 결과값 보여주는 함수
 function viewResult(resultvalue) {
-    document.getElementById('result').value = resultvalue;
+  document.getElementById('result').value = resultvalue;
 }
 
 // 연산기호 연속입력 막는 함수
 function insertKey(keye) {
-    if (oparry.includes(history[history.length - 1])) {
-      if (!oparry.includes(keye)) {
-        combineArray(keye)
-      }
-    } else {
-      combineArray(keye);
+  if (oparry.includes(history[history.length - 1])) {
+    if (!oparry.includes(keye)) {
+      combineArray(keye)
     }
+  } else {
+    combineArray(keye);
+  }
 }
 
 // 입력한 값에따라 그에 맞는 함수실행
 function combineArray(keye) {
-    if (["Enter", "="].includes(keye)) {
-      calculaterResult();
-    } else if ("Backspace" === keye){
-      deleteElement();
+  if (["Enter", "="].includes(keye)) {
+    calculaterResult();
+  } else if ("Backspace" === keye) {
+    deleteElement();
+  } else {
+    history.push(keye);
+    if (oparry.includes(keye)) {
+      arr.push(Number(cnsctNmbrs.splice(0, cnsctNmbrs.length).join("")), keye);
     } else {
-      history.push(keye);
-      if (oparry.includes(keye)) {
-        arr.push(Number(cnsctNmbrs.splice(0, cnsctNmbrs.length).join("")), keye);
-      } else {
-        cnsctNmbrs.push(keye)
-      }
+      cnsctNmbrs.push(keye)
     }
   }
-  
+}
+
 
 // 입력값에 연산자가 있는지 확인
-function hasOperator(opers){
+function hasOperator(opers) {
   for (let i = 0; i < arr.length; i++) {
     if (opers.includes(arr[i])) {
       return true;
@@ -77,20 +77,20 @@ function hasOperator(opers){
 // 곱하기 우선 연산 및 곱하기연산
 function repeatMultiplydivision() {
   // 곱셈나눗셈, 덧셈뺄셈 섞인연산
-  while (true){
+  while (true) {
     const firstCaseIndex = arr.findIndex((i) => ['*', '/'].includes(i))
     if (firstCaseIndex === -1) {
       break;
     }
     const [fir, op, la] = arr.splice(firstCaseIndex - 1, 3);
     resultnumber = op === '*' ? fir * la : fir / la;
-    if (hasOperator(['+','-'])) {
+    if (hasOperator(['+', '-'])) {
       arr.splice(firstCaseIndex - 1, 0, resultnumber)
     } else {
-      if (hasOperator(['*','/'])) {
+      if (hasOperator(['*', '/'])) {
         cnsctNmbrs.push(resultnumber)
-        calculatePlusminers(['*','/']);
-       } else {
+        calculatePlusminers(['*', '/']);
+      } else {
         viewResult(resultnumber)
       }
       return
@@ -98,20 +98,20 @@ function repeatMultiplydivision() {
   }
 
   // 더하기뺴기만 있는연산
-  while (true){
+  while (true) {
     const firstCaseIndex = arr.findIndex((i) => ['+', '-'].includes(i))
     if (firstCaseIndex === -1) {
       break;
     }
     const [fir, op, la] = arr.splice(firstCaseIndex - 1, 3);
     resultnumber = op === '+' ? fir + la : fir - la;
-      if (hasOperator(['+','-'])) {
-        cnsctNmbrs.push(resultnumber)
-        calculatePlusminers(['+','-']);
-      } else {
-        viewResult(resultnumber)
-      }
-      return
+    if (hasOperator(['+', '-'])) {
+      cnsctNmbrs.push(resultnumber)
+      calculatePlusminers(['+', '-']);
+    } else {
+      viewResult(resultnumber)
+    }
+    return
   }
 }
 
@@ -144,24 +144,24 @@ function calculatePlusminers(opsymbol) {
 
 // backspace 지우기
 function deleteElement() {
-    if (!oparry.includes(history[history.length - 1])) {
-      history.pop();
-      cnsctNmbrs.pop();
-    }
+  if (!oparry.includes(history[history.length - 1])) {
+    history.pop();
+    cnsctNmbrs.pop();
+  }
 }
 
 // enter 눌렀을때 연산시작
 function calculaterResult() {
- 
-    arr.push(Number(cnsctNmbrs.splice(0,cnsctNmbrs.length).join("")));
-    history.length = 0;
-    repeatMultiplydivision();
-    arr.length = 0;
-    const availNumber = !isNaN(resultnumber) && !Number.isInteger(resultnumber);
-    cnsctNmbrs.push(availNumber ? Number(resultnumber).toFixed(1) : resultnumber);
-    history.push(availNumber ?  Number(resultnumber).toFixed(1) : resultnumber);
-    viewResult(availNumber ?  Number(resultnumber).toFixed(1) : resultnumber);
-    event.preventDefault()
+
+  arr.push(Number(cnsctNmbrs.splice(0, cnsctNmbrs.length).join("")));
+  history.length = 0;
+  repeatMultiplydivision();
+  arr.length = 0;
+  const availNumber = !isNaN(resultnumber) && !Number.isInteger(resultnumber);
+  cnsctNmbrs.push(availNumber ? Number(resultnumber).toFixed(1) : resultnumber);
+  history.push(availNumber ? Number(resultnumber).toFixed(1) : resultnumber);
+  viewResult(availNumber ? Number(resultnumber).toFixed(1) : resultnumber);
+  event.preventDefault()
 
 }
 
